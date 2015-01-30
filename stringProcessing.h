a72ed0c759a5f8ccf7ba074652bea522ec66acf7
jwchen <jwchen@jwchen-ThinkPad-X230.(none)>
2015-01-30 15:12:51 -0800

    remove duplicate getCommand etc

--- stringProcessing.h
+++ stringProcessing.h
@@ -3,3 +3,4 @@
 extern int getCommand(char* commLine, Method* method, char* fname, HttpVersion *version);
 extern FileType getFileType(char *fname);
 extern int removeDotSegments(char source[]);
+extern const char defaultPage[];
